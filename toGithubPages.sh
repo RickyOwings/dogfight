e3c973ca0a6c4946dee15442299e7dfd601f0299
RickyOwings <rickyowings@gmail.com>
2023-07-22 17:22:33 -0400

    Added more functionality to the shell automation script. Builds the static page, updates the sounds json, and pushes to both repositories!

--- toGithubPages.sh
+++ toGithubPages.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+read -p "Commit message: " comMes
+
+node updateMusicJSON.cjs
 npx snowpack build
-git subtree add --prefix build origin gh-pages
+git add .
+git commit -m "$comMes"
+git push
 git subtree push --prefix build origin gh-pages
--- updateMusicJSON.cjs
+++ updateMusicJSON.cjs
@@ -2,6 +2,7 @@
 const fs = require('fs');
 fs.readdir('./public/assets/sounds/', (err, files)=>{ 
     if (err) console.log(err);
+    console.log("Reading files in sounds directory and updating the json")
     fs.writeFile('./public/assets/sounds/sounds.json', JSON.stringify(files), err => {
         if (err) console.log(err);
     })
